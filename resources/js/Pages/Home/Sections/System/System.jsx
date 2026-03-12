fbb24ec2d9ba324aa542ad66917faa94cbd94bd0
oncelerr <markjonathan368@gmail.com>
2026-03-12 16:08:14 +0800

    fixed images

--- resources/js/Pages/Home/Sections/System/System.jsx
+++ resources/js/Pages/Home/Sections/System/System.jsx
@@ -27,9 +27,9 @@ const System = () => {
         Not just websites. Not just design. Complete digital foundations engineered for one thing: results.
       </p>
       <div className={styles.systemCardCont} ref={cardContRef}>
-        <Card img="/assets/cardimg1.png" text="Clarity" description="Audiences instantly know who the brand is and what action to take next through strategic positioning, intuitive experience design, and clear visual messaging." />
-        <Card img="/assets/cardimg2.png" text="Conversion" description="Traffic converts instead of leaking through conversion-focused architecture, performance-optimized development, and SEO-ready structure from day one." />
-        <Card img="/assets/cardimg3.png" text="Scale-Readiness" description="Growth happens without the need to rebuild thanks to flexible CMS, built-in analytics and tracking, and clean modular architecture." />
+        <Card img="/Assets/cardimg1.png" text="Clarity" description="Audiences instantly know who the brand is and what action to take next through strategic positioning, intuitive experience design, and clear visual messaging." />
+        <Card img="/Assets/cardimg2.png" text="Conversion" description="Traffic converts instead of leaking through conversion-focused architecture, performance-optimized development, and SEO-ready structure from day one." />
+        <Card img="/Assets/cardimg3.png" text="Scale-Readiness" description="Growth happens without the need to rebuild thanks to flexible CMS, built-in analytics and tracking, and clean modular architecture." />
       </div>
     </div>
   );
